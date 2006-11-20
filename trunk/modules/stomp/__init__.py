a84bad10dfc132768a6856c2a1a5bf7dbe05cf11
petry <petry@b966eb11-e821-0410-a270-e8050b38c6a6>
2006-11-20 15:11:36 +0000

    added python module: stomp

---
+++ trunk/modules/stomp/__init__.py
@@ -0,0 +1,8 @@
+#######################################
+# Alexander Petry <petry@itwm.fhg.de> #
+# Copyright 2006                      #
+#######################################
+
+from Connection import Connection
+__version__ = '0.1'
+
