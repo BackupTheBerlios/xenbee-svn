280e3202b71b9abb5da5be2d4cde8244be4b3a1c
petry <petry@b966eb11-e821-0410-a270-e8050b38c6a6>
2007-02-22 18:42:17 +0000

    * documented the fsm a little bit

--- trunk/modules/xbe/xbed/task_fsm.py
+++ trunk/modules/xbe/xbed/task_fsm.py
@@ -1,11 +1,13 @@
 """A module, that simply contains the Finite State Machine of a task."""
 
-
 from xbe.util import fsm
 
 class TaskFSM(object):
     def __init__(self):
-        """Initialize a new task fsm."""
+        """Initialize a new task fsm.
+
+        Have a look at the source code for a pictogram of the state machine.
+        """
         m = fsm.FSM()
         m.newState("Failed")                      # the task failed
         m.newState("Terminated")                  # the task has been terminated by the user
@@ -33,7 +35,7 @@ class TaskFSM(object):
         m.setStartState("Pending:Reserved")
         m.addTransition("Pending:Reserved",    # src
                         "Pending:Confirmed",   # dst
-                        "confirm_token", self.do_confirmed) # trigger
+                        "confirm_token", self.do_confirmed) # trigger and transition function
         m.addTransition("Pending:Reserved",
                         "Terminated",
                         "terminate_token", self.do_terminate_pending_reserved)
@@ -43,8 +45,6 @@ class TaskFSM(object):
         m.addTransition("Pending:Confirmed",
                         "Running:Stage-In",
                         "start_token", self.do_stage_in)
-#
-#
 #
 #                                 +----------+
 #                +--------------->|  Failed  |<--------------+------------------+----------+
@@ -61,7 +61,7 @@ class TaskFSM(object):
 #                |             +-------------+                |                             |
 #                `------------>| Terminated  |<---------------+-----------------------------+
 #                              +-------------+
-#
+
         m.addTransition("Running:Stage-In",
                         "Terminated",
                         "terminate_token", self.do_terminate_stage_in)
@@ -123,113 +123,139 @@ class TaskFSM(object):
     # 'external' transition trigger  methods
     # those are triggers called from another source, such as a network
     # protocol stack
-    def failed(self, reason):
-        """the task has failed, reason should be a twisted.python.failure.Failure."""
-        return self.fsm.consume("fail_token", reason)
     def terminate(self, reason=None):
         """user requests the termination."""
         return self.fsm.consume("terminate_token", reason)
 
-    def confirm(self):
+    def confirm(self, *args, **kw):
         """user confirmed the task"""
-        self.fsm.consume("confirm_token")
-    def start(self):
+        self.fsm.consume("confirm_token", *args, **kw)
+    def start(self, *args, **kw):
         """initiate the starting of the task."""
-        self.fsm.consume("start_token")
-    def execute(self):
+        self.fsm.consume("start_token", *args, **kw)
+    def execute(self, *args, **kw):
         """triggers the execution of the task."""
-        self.fsm.consume("execute_task_token")
+        self.fsm.consume("execute_task_token", *args, **kw)
+
+    # the  only errback to  be used,  it will  be transformed  into an
+    # appropriate errback using the fsm
+    def failed(self, reason):
+        """The task has failed, reason should be a twisted.python.failure.Failure.
+
+        An appropriate method that depends on the current state will be called.
+        """
+        self.fsm.consume("fail_token", reason)
 
     # callbacks, that will trigger FSM transitions
-    # you can use these to trigger state changes
+    # you can use them to trigger state changes
     def cb_stage_in_completed(self, *args, **kw):
-        self.fsm.consume("stage_in_completed_token")
+        self.fsm.consume("stage_in_completed_token", *args, **kw)
     def cb_instance_started(self, *args, **kw):
-        self.fsm.consume("instance_started_token")
+        self.fsm.consume("instance_started_token", *args, **kw)
     def cb_execution_finished(self, *args, **kw):
-        self.fsm.consume("execution_finished_token")
+        self.fsm.consume("execution_finished_token", *args, **kw)
     def cb_instance_stopped(self, *args, **kw):
-        self.fsm.consume("instance_stopped_token")
+        self.fsm.consume("instance_stopped_token", *args, **kw)
     def cb_stage_out_completed(self, *args, **kw):
-        self.fsm.consume("stage_out_completed_token")
+        self.fsm.consume("stage_out_completed_token", *args, **kw)
         
     # FSM transitions override these in a subclass to add functionality
     def do_confirmed(self, *args, **kw):
+        """The task has been confirmed by the user."""
         pass
 
     def do_terminate_pending_reserved(self, reason, *args, **kw):
+        """terminate a pending task, that has not been confirmed yet.
+
+        That should not impose much work to do.
+        """
         pass
 
     def do_terminate_pending_confirmed(self, reason, *args, **kw):
+        """terminate a confirmed task."""
         pass
 
     def do_stage_in(self, *args, **kw):
-        # this method must be asynchronous
-        # addCallback(self.stage_in_completed).addErrback(self.failed)
-        # pre-cond: task description and non-file resources available
-        # post-cond: state == Running:Stage-In
+        """stage in all necessary files and wait for other resources.
+
+        this method must be asynchronous
+        addCallback(self.stage_in_completed).addErrback(self.failed)
+
+        pre-cond: task description available and task has been confirmed
+        post-cond: state == Running:Stage-In
+        """
         pass
 
     def do_terminate_stage_in(self, reason, *args, **kw):
-        # terminate the stage-in process, must not fail
+        """terminate the stage-in process, must not fail."""
         pass
 
     def do_stage_in_failed(self, reason, *args, **kw):
-        # handle the case, that the staging failed
+        """handle the case, that the staging failed"""
         pass
 
     def do_start_instance(self, *args, **kw):
-        # all files are staged in, so trigger the start of the instance
-        # addCallback(self.instance_started).addErrback(self.failed)
+        """all files are staged in, so trigger the start of the instance
+           addCallback(self.cb_instance_started).addErrback(self.failed)"""
         pass
 
     def do_terminate_instance_starting(self, reason, *args, **kw):
+        """terminate the start process of the instance."""
         pass
 
     def do_instance_starting_failed(self, reason, *args, **kw):
-        # handle a failed instance start attempt
+        """handle a failed instance start attempt"""
         pass
 
     def do_execute_task(self, *args, **kw):
-        # pre-cond. instance available
-        # post-cond. jsdl sent to instance
+        """execute the task
+        pre-cond. instance available -> protocol is ready
+        post-cond. jsdl sent to instance
+        """
         pass
 
     def do_terminate_execution(self, reason, *args, **kw):
-        # terminate the execution within the instance
+        """terminate the execution within the instance.
+
+        1. terminate the task within the instance
+        2. shutdown the instance
+        """
         pass
 
     def do_execution_failed(self, reason, *args, **kw):
-        # the task execution failed
+        """the task execution failed
+
+        that should be something like 'do_stop_instance' with different callbacks
+        """
         pass
 
     def do_stop_instance(self, *args, **kw):
-        # stop the instance
+        """the task has finished its execution stop the instance"""
         pass
 
     def do_terminate_instance_stopping(self, reason, *args, **kw):
-        # terminate the shutdown-process
-        # I think that has to be a 'noop', but the 'instance-stopped' callback
-        # must not do any harm!
+        """terminate the shutdown-process
+        I think that has to be a 'noop', but the 'instance-stopped' callback
+        must not do any harm!"""
         pass
 
     def do_instance_stopping_failed(self, reason, *args, **kw):
-        # i really do not know, what to do about that, maybe force the destroying
+        """i really do not know, what to do about that, maybe force the destroying"""
         pass
 
     def do_stage_out(self, *args, **kw):
-        # task has finished its execution,
-        # instance has been shut down, stage out the data
+        """task has finished its execution,
+           instance has been shut down, stage out the data"""
         pass
 
     def do_stage_out_failed(self, reason, *args, **kw):
-        # stage out process failed, so handle that
+        """stage out process failed, so handle that"""
         pass
 
     def do_terminate_stage_out(self, *args, **kw):
-        # user requested the termination of this task
+        """terminate the stage out process"""
         pass
 
     def do_task_finished(self):
-        # pre-cond: stage-out is complete
+        """pre-cond: stage-out is complete"""
         pass
