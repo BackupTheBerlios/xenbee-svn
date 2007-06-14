3ebf6b66c4136062cace879d0b008c3d4be3f075
petry <petry@b966eb11-e821-0410-a270-e8050b38c6a6>
2007-06-14 11:31:05 +0000

    * creating new 0.1-stable release (step 1)

--- branches/0.1-stable/modules/xbe/xbed/config/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Xen config generator
-"""
-
-__version__ = "$Rev$"
-__author__ = "$Author: petry $"
-
-from test_config import suite as TestSuite
--- branches/0.1-stable/modules/xbe/xml/__init__.py
+++
@@ -1,9 +0,0 @@
-"""The xml module."""
-
-from lxml import etree
-import copy
-
-def cloneDocument(doc):
-    return copy.deepcopy(doc)
-
-__all__ = [ "cloneDocument" ]
