6db4ba1ffc4e264bbbfe53c84345fc5d891987f7
petry <petry@b966eb11-e821-0410-a270-e8050b38c6a6>
2007-06-14 11:26:43 +0000

    * Added Copyright information to the top of each source file

--- trunk/modules/xbe/xbed/task_fsm.py
+++ trunk/modules/xbe/xbed/task_fsm.py
@@ -1,3 +1,25 @@
+# XenBEE is a software that provides execution of applications
+# in self-contained virtual disk images on a remote host featuring
+# the Xen hypervisor.
+#
+# Copyright (C) 2007 Alexander Petry <petry@itwm.fhg.de>.
+# This file is part of XenBEE.
+
+# XenBEE is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# XenBEE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA
+
 """A module, that simply contains the Finite State Machine of a task."""
 
 from xbe.util import fsm
