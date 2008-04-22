a3cdafe522ca42994563c7aa3bf1066f3be1e111
petry <petry@b966eb11-e821-0410-a270-e8050b38c6a6>
2008-04-22 12:52:20 +0000

    * added TODO list * added method skeleton for PingPong TestCase

--- xbeinstd/branches/cmake-and-splitting/xbe/tests/xbe/PingPong.cpp
+++ xbeinstd/branches/cmake-and-splitting/xbe/tests/xbe/PingPong.cpp
@@ -1,16 +1,42 @@
+#include <xbe/ChannelEventQueueAdapter.hpp>
+#include <mqs/Channel.hpp>
+
 #include "PingPong.hpp"
 
 using namespace tests::xbe;
 
-PingPong::PingPong() {
-
-}
+PingPong::PingPong(const seda::Strategy::Ptr& s, const seda::Stage::Ptr& out, bool initialSend)
+    : ::xbe::XMLMessageDispatcher(s), _fsm(*this), _out(out), _initialSend(initialSend)
+{ }
 
 PingPong::~PingPong() {}
 
-void PingPong::start() {}
+void PingPong::start() {
+    if (_initialSend) {
+        // generate a new PingEvent and send it to 'out'
+    }
+}
 
-void PingPong::stop() {}
+void PingPong::dispatch(const xbemsg::message_t& msg) {
+    // analyse type of message
+    // if (typeof(msg) == PongEvent)
+    //    _fsm.Ping(msg)
+    // else if (typeof(msg) == PingEvent)
+    //    _fsm.Pong(msg)
+    // else
+    //    log erroneous message arrival
+}
 
-void PingPong::sendPong(const tests::xbe::PingEvent& m) {}
-void PingPong::sendPing(const tests::xbe::PongEvent& m) {}
+void PingPong::stop() {
+    
+}
+
+void PingPong::sendPong(const tests::xbe::PingEvent& m) {
+    // PongEvent pong(...);
+    // _out->send(pong);
+}
+
+void PingPong::sendPing(const tests::xbe::PongEvent& m) {
+    // PingEvent ping(...);
+    // _out->send(ping);
+}
--- xbeinstd/branches/cmake-and-splitting/xbe/tests/xbe/PingPong.hpp
+++ xbeinstd/branches/cmake-and-splitting/xbe/tests/xbe/PingPong.hpp
@@ -1,6 +1,10 @@
 #ifndef TESTS_XBE_PING_PONG_HPP
 #define TESTS_XBE_PING_PONG_HPP 1
 
+#include <seda/Stage.hpp>
+#include <seda/Strategy.hpp>
+#include <xbe/XMLMessageDispatcher.hpp>
+
 #include <tests/xbe/PingEvent.hpp>
 #include <tests/xbe/PongEvent.hpp>
 
@@ -8,9 +12,9 @@
 
 namespace tests {
     namespace xbe {
-        class PingPong {
+        class PingPong : ::xbe::XMLMessageDispatcher {
         public:
-            PingPong();
+            PingPong(const seda::Strategy::Ptr& s, const seda::Stage::Ptr& out, bool initialSend=false);
             ~PingPong();
 
         public: // StateMachine callbacks
@@ -18,9 +22,18 @@ namespace tests {
             void sendPing(const tests::xbe::PongEvent&);
             void sendPong(const tests::xbe::PingEvent&);
             void stop();
+
+        protected: // message dispatcher
+            virtual void dispatch(const xbemsg::message_t& msg);
+            
+        protected:
+            PingPongContext _fsm;  // what to do with incoming events
+            seda::Stage::Ptr _out; // where to send generated events
+
+        private:
+            bool _initialSend; // perform an inital send operation during start up
         };
     }
 }
-
 
 #endif // ! TESTS_XBE_PING_PONG_HPP
