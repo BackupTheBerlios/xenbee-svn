e6d831d16dddefdbccdb45dfba6cc6a600a8e226
gonium <gonium@b966eb11-e821-0410-a270-e8050b38c6a6>
2008-05-13 08:07:55 +0000

    added debugging output to FindXsd.cmake

--- xbeinstd/branches/cmake-and-splitting/xbe/modules/FindXsd.cmake
+++ xbeinstd/branches/cmake-and-splitting/xbe/modules/FindXsd.cmake
@@ -7,6 +7,10 @@
 # XSD_EXECUTABLE, where is the xsd compiler
 # XSD_FOUND, If false, don't try to use xsd
 
+set(ADDITIONAL_XSD_PATH $ENV{XSDDIR})
+#debugging
+MESSAGE("XSD additional search path is: ${ADDITIONAL_XSD_PATH}")
+
 FIND_PATH(XSD_INCLUDE_DIR xsd/cxx/parser/elements.hxx
   "[HKEY_CURRENT_USER\\software\\xsd\\include]"
   "[HKEY_CURRENT_USER]\\xsd\\include]"
@@ -20,7 +24,7 @@ FIND_PROGRAM(XSD_EXECUTABLE
   NAMES 
     xsd
   PATHS
-    "[HKEY_CURRENT_USER\\xsd\\bin"
+    "[HKEY_CURRENT_USER]\\xsd\\bin"
     $ENV{XSDDIR}/bin
     /usr/local/bin
     /usr/bin
@@ -28,6 +32,9 @@ FIND_PROGRAM(XSD_EXECUTABLE
   DOC "Path to the CodeSynthesis Schema Compiler"
   NO_DEFAULT_PATH
 )
+
+MESSAGE("Found XSD executable: ${XSD_EXECUTABLE}")
+
 
 # if the include and the program are found then we have it
 IF(XSD_INCLUDE_DIR)
