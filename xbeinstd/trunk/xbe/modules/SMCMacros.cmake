3b76987f35745ab2a31258922a863e93fe848ea3
petry <petry@b966eb11-e821-0410-a270-e8050b38c6a6>
2008-10-29 15:25:47 +0000

    - fixed the distclean target of the toplevel Makefiles - fixed the Xsd and Smc macros to regenerate the files only if needed

--- xbeinstd/trunk/xbe/modules/SMCMacros.cmake
+++ xbeinstd/trunk/xbe/modules/SMCMacros.cmake
@@ -5,13 +5,15 @@
 
 function(add_state_machine FSM_NAME)
   if(SMC_FOUND)
-    add_custom_command(OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/${FSM_NAME}_sm.cpp ${CMAKE_CURRENT_SOURCE_DIR}/${FSM_NAME}_sm.h
-      COMMAND ${JAVA_RUNTIME} -jar ${SMC_JAR} -c++ ${CMAKE_CURRENT_SOURCE_DIR}/${FSM_NAME}.sm
-      COMMAND ${CMAKE_COMMAND} -E touch_nocreate ${CMAKE_CURRENT_SOURCE_DIR}/${FSM_NAME}_sm.cpp
-      COMMAND ${CMAKE_COMMAND} -E touch_nocreate ${CMAKE_CURRENT_SOURCE_DIR}/${FSM_NAME}_sm.h
-      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-      MAIN_DEPENDENCY ${CMAKE_CURRENT_SOURCE_DIR}/${FSM_NAME}.sm
-      COMMENT "Compiling '${FSM_NAME}' state machine...")
+    if ("${CMAKE_CURRENT_SOURCE_DIR}/${FSM_NAME}.sm" IS_NEWER_THAN "${CMAKE_CURRENT_SOURCE_DIR}/${FSM_NAME}_sm.cpp")
+        add_custom_command(OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/${FSM_NAME}_sm.cpp ${CMAKE_CURRENT_SOURCE_DIR}/${FSM_NAME}_sm.h
+          COMMAND ${JAVA_RUNTIME} -jar ${SMC_JAR} -c++ ${CMAKE_CURRENT_SOURCE_DIR}/${FSM_NAME}.sm
+          COMMAND ${CMAKE_COMMAND} -E touch_nocreate ${CMAKE_CURRENT_SOURCE_DIR}/${FSM_NAME}_sm.cpp
+          COMMAND ${CMAKE_COMMAND} -E touch_nocreate ${CMAKE_CURRENT_SOURCE_DIR}/${FSM_NAME}_sm.h
+          WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+          MAIN_DEPENDENCY ${CMAKE_CURRENT_SOURCE_DIR}/${FSM_NAME}.sm
+          COMMENT "Compiling '${FSM_NAME}' state machine...")
+    endif ("${CMAKE_CURRENT_SOURCE_DIR}/${FSM_NAME}.sm" IS_NEWER_THAN "${CMAKE_CURRENT_SOURCE_DIR}/${FSM_NAME}_sm.cpp")
   else(SMC_FOUND)
     if ("${CMAKE_CURRENT_SOURCE_DIR}/${FSM_NAME}.sm" IS_NEWER_THAN "${CMAKE_CURRENT_SOURCE_DIR}/${FSM_NAME}_sm.cpp")
       message(FATAL_ERROR "The '${FSM_NAME}' StateMachine needs to be updated but the StateMachineCompiler (SMC) could not be found!")
--- xbeinstd/trunk/xbe/modules/XsdMacros.cmake
+++ xbeinstd/trunk/xbe/modules/XsdMacros.cmake
@@ -7,21 +7,23 @@ set(XSD_ARGS "cxx-tree" "--generate-serialization" "--hxx-suffix" ".hpp" "--cxx-
 
 macro(add_xsd_schema BASE_NAME PATH_TO_SCHEMA)
   if(XSD_FOUND)
-    add_custom_command(OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/${BASE_NAME}.cpp ${CMAKE_CURRENT_SOURCE_DIR}/${BASE_NAME}.hpp
-      COMMAND ${XSD_EXECUTABLE}
-      ARGS ${XSD_ARGS} --reserved-name "LINUX=LINUX_OS"
-      --namespace-map "http://www.xenbee.net/schema/2008/02/xbe-msg=xbemsg"
-      --namespace-map "http://schemas.ggf.org/jsdl/2005/11/jsdl-posix=jsdlPosix"
-      --namespace-map "http://www.w3.org/2000/09/xmldsig#=dsig"
-      --namespace-map "http://www.w3.org/2001/04/xmlenc#=xenc"
-      --location-map "http://www.w3.org/TR/2002/REC-xmldsig-core-20020212/xmldsig-core-schema.xsd=dsig.xsd"
-      --location-map "http://schemas.ggf.org/jsdl/2005/11/jsdl=jsdl.xsd"
-      --location-map "http://www.w3.org/TR/2002/REC-xmlenc-core-20021210/xenc-schema.xsd=xenc.xsd"
-      ${PATH_TO_SCHEMA}
-      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-      MAIN_DEPENDENCY ${PATH_TO_SCHEMA}
-      DEPENDS ${PATH_TO_SCHEMA}
-      COMMENT "Generating '${BASE_NAME}' schema data binding...")
+    if ("${PATH_TO_SCHEMA}" IS_NEWER_THAN "${CMAKE_CURRENT_SOURCE_DIR}/${BASE_NAME}.cpp")
+        add_custom_command(OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/${BASE_NAME}.cpp ${CMAKE_CURRENT_SOURCE_DIR}/${BASE_NAME}.hpp
+          COMMAND ${XSD_EXECUTABLE}
+          ARGS ${XSD_ARGS} --reserved-name "LINUX=LINUX_OS"
+          --namespace-map "http://www.xenbee.net/schema/2008/02/xbe-msg=xbemsg"
+          --namespace-map "http://schemas.ggf.org/jsdl/2005/11/jsdl-posix=jsdlPosix"
+          --namespace-map "http://www.w3.org/2000/09/xmldsig#=dsig"
+          --namespace-map "http://www.w3.org/2001/04/xmlenc#=xenc"
+          --location-map "http://www.w3.org/TR/2002/REC-xmldsig-core-20020212/xmldsig-core-schema.xsd=dsig.xsd"
+          --location-map "http://schemas.ggf.org/jsdl/2005/11/jsdl=jsdl.xsd"
+          --location-map "http://www.w3.org/TR/2002/REC-xmlenc-core-20021210/xenc-schema.xsd=xenc.xsd"
+          ${PATH_TO_SCHEMA}
+          WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+          MAIN_DEPENDENCY ${PATH_TO_SCHEMA}
+          DEPENDS ${PATH_TO_SCHEMA}
+          COMMENT "Generating '${BASE_NAME}' schema data binding...")
+    endif("${PATH_TO_SCHEMA}" IS_NEWER_THAN "${CMAKE_CURRENT_SOURCE_DIR}/${BASE_NAME}.cpp")
   else(XSD_FOUND)
     if ("${PATH_TO_SCHEMA}" IS_NEWER_THAN "${CMAKE_CURRENT_SOURCE_DIR}/${BASE_NAME}.cpp")
       message(FATAL_ERROR "Data binding for the ${BASE_NAME} schema needs to be updated but XSD could not be found!")
