bfe8bb2a6adfce5c201b94a9f6afd4199588f22d
petry <petry@b966eb11-e821-0410-a270-e8050b38c6a6>
2009-01-28 11:17:47 +0000

    removed debug outputs from ForwardStrategy

--- xbeinstd/trunk/seda/seda/ForwardStrategy.cpp
+++ xbeinstd/trunk/seda/seda/ForwardStrategy.cpp
@@ -6,17 +6,12 @@
 #include <iostream>
 
 namespace seda {
-  void ForwardStrategy::perform(const IEvent::Ptr& e) {
-      const Stage::Ptr& nextStage(StageRegistry::instance().lookup(_next));
-      if (nextStage) {
-        std::cout << "nasty line here." << std::endl;
-        std::cout << "talking to: "<< _next << std::endl;
-          //SEDA_LOG_WARN("forwarding event to stage `" << nextStage->name() << "'");
-        std::cout << "after log." << std::endl;
-          nextStage->send(e);
-        std::cout << "after send." << std::endl;
-      } else {
-          SEDA_LOG_WARN("nothing to forward to!");
-      }
-  }
+    void ForwardStrategy::perform(const IEvent::Ptr& e) {
+        const Stage::Ptr& nextStage(StageRegistry::instance().lookup(_next));
+        if (nextStage) {
+            nextStage->send(e);
+        } else {
+            SEDA_LOG_WARN("nothing to forward to!");
+        }
+    }
 }
