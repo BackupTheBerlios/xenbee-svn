19748d5ccd06d9525a1b1862d5e6fe37e5baecf3
petry <petry@b966eb11-e821-0410-a270-e8050b38c6a6>
2009-02-05 12:58:52 +0000

    The DiscardStrategy accepts a name parameter now (defaults to "discard")

--- xbeinstd/trunk/seda/seda/DiscardStrategy.hpp
+++ xbeinstd/trunk/seda/seda/DiscardStrategy.hpp
@@ -6,15 +6,17 @@
 #include <seda/Strategy.hpp>
 
 namespace seda {
-  class DiscardStrategy : public Strategy {
-  public:
-    DiscardStrategy() : Strategy("discard") {}
-    ~DiscardStrategy() {}
+    class DiscardStrategy : public Strategy {
+        public:
+            explicit
+                DiscardStrategy(const std::string &name = "discard")
+                : Strategy(name) {}
+            ~DiscardStrategy() {}
 
-    void perform(const IEvent::Ptr& e) {
-        SEDA_LOG_DEBUG("discarding: " << e->str());
-    }
-  };
+            void perform(const IEvent::Ptr& e) {
+                SEDA_LOG_DEBUG("discarding: " << e->str());
+            }
+    };
 }
 
 #endif // !SEDA_DISCARD_STRATEGY_HPP
