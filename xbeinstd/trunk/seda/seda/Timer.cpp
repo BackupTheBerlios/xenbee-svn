9c14d1f4c459626cde28254a78e1f99d84c23e77
petry <petry@b966eb11-e821-0410-a270-e8050b38c6a6>
2009-03-23 14:14:58 +0000

    * moved the xbeinstd test to an own test-suite

--- xbeinstd/trunk/seda/seda/Timer.cpp
+++ xbeinstd/trunk/seda/seda/Timer.cpp
@@ -10,17 +10,17 @@ Timer::start() {
 
     if (_active)
         return;
-    _active = true;
     _thread = boost::thread(boost::ref(*this));
 }
 
 void
 Timer::stop() {
     boost::unique_lock<boost::recursive_mutex> lock(_mtx);
-    _active = false;
     _thread.interrupt();     
-    while (_thread.get_id() != boost::thread::id())
+    while (_thread.get_id() != boost::thread::id()) {
         _cond.wait(lock);
+    }
+    _active = false;
 }
 
 bool
@@ -31,14 +31,22 @@ Timer::active() {
 
 void
 Timer::operator()() {
+    {
+        boost::unique_lock<boost::recursive_mutex> lock(_mtx);
+        if (_active) {
+            return;
+        } else {
+            _active = true;
+        }
+    }
+
     while (active()) {
         try {
             boost::this_thread::sleep(boost::get_system_time() + interval());
 
-            seda::TimerEvent::Ptr evt(new seda::TimerEvent(tag()));
             try {
                 seda::Stage::Ptr stage(StageRegistry::instance().lookup(targetStage()));
-                stage->send(evt);
+                stage->send(seda::TimerEvent::Ptr(new seda::TimerEvent(tag())));
             } catch(const seda::StageNotFound& ) {
                 std::clog << "stage `" << targetStage() << "' could not be found!" << std::endl;
                 break;
@@ -49,8 +57,10 @@ Timer::operator()() {
             break;
         }
     }
+
     boost::unique_lock<boost::recursive_mutex> lock(_mtx);
     _thread.detach();
     _cond.notify_one();
+    _active = false;
 }
 
