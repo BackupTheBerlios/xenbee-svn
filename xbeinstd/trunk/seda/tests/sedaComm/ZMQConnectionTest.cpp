04f9248faa7f95aa726d396aea20a8677e5544d9
petry <petry@b966eb11-e821-0410-a270-e8050b38c6a6>
2009-07-06 14:01:17 +0000

    added a loop around start() stop() calls which segfaults for large (probably > 1) iterations. This is a problem of libzmq ;-(

--- xbeinstd/trunk/seda/tests/sedaComm/ZMQConnectionTest.cpp
+++ xbeinstd/trunk/seda/tests/sedaComm/ZMQConnectionTest.cpp
@@ -50,20 +50,24 @@ void
 ZMQConnectionTest::testStartStop() {
   seda::comm::ZMQConnection conn("localhost", "test", "*:5222", "*");
   try {
-    conn.start();
-    conn.stop();
-    conn.start();
+    // FIXME: this segfaults for larger loop-counts! problem of libzmq!
+    for (std::size_t i(0); i < 1; i++) {
+      conn.start();
+
+      seda::comm::SedaMessage msg1("test", "test", "foo");
+      conn.send(msg1);
+      seda::comm::SedaMessage msg2;
+      conn.receive(msg2);
+      CPPUNIT_ASSERT_MESSAGE("received payload differs from sent payload", msg1.payload() == msg2.payload());
+
+      conn.stop();
+    }
   } catch (const std::exception &ex) {
     SEDA_LOG_ERROR("could not start the zmq-connection: " << ex.what());
     CPPUNIT_ASSERT_MESSAGE("zmq connection could not be started", false);
   } catch(...) {
     CPPUNIT_ASSERT_MESSAGE("zmq connection could not be started", false);
   }
-  seda::comm::SedaMessage msg1("test", "test", "foo");
-  conn.send(msg1);
-  seda::comm::SedaMessage msg2;
-  conn.receive(msg2);
-  CPPUNIT_ASSERT_MESSAGE("received payload differs from sent payload", msg1.payload() == msg2.payload());
 }
 
 
